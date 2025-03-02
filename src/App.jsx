4e0d3dc20f426846ef06985bc86a8da179227265
kirushikan0508 <kirushikankiru01@gmail.com>
2025-03-02 21:40:19 +0530

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/programs/programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
-      <Programs/>
+      <div className='container'>
+        <Title subTitle='Our PROGRAM' title='What We Offer'/>
+        <Programs/>
+      </div>
+      
     </div>
   )
 }
